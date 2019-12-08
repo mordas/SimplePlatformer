884eadf1aab4efec6d315532972708b8152a9386
dMordas <dontbeatmybrain@gmail.com>
2019-12-08 15:26:53 +0300

    Базовые движения персонажа, прыжки, двойные прыжки + монеты

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")){
+other.GetComponent<Player>().CountScore(1);
+Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,49 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController _controller;
-    [SerializeField] private float _speed;
-    
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _gravity = 1.0f;
+    [SerializeField] private float _jumpHeight = 15.0f;
+    private bool _canDoubleJump = false;
+    private int _score = 0;
+
+    [SerializeField] private GameObject _uiManager;
+private float _yVelocity;
     // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();
-    
     }
-    
 
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 direction = new Vector3(horizontalInput,0,0);
-        _controller.Move(direction * Time.deltaTime * _speed);
-        
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * _speed;
+
+        if (!_controller.isGrounded)
+        {
+            if(Input.GetKeyDown(KeyCode.Space) && _canDoubleJump){
+                _yVelocity += _jumpHeight;
+            _canDoubleJump = false;
+            }
+            _yVelocity -= _gravity;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+        }
+        velocity.y = _yVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+
+    public void CountScore(int score){
+_score += score;
+_uiManager.gameObject.GetComponent<UIManager>().UpdateCoins(_score);
     }
 }
